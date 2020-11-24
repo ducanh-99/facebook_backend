71a5718014b4427cc20f937fcd26af1a72f74349
ducanh <ducanhhust99@gmail.com>
2020-11-24 16:22:09 +0700

    fix connect

--- app/config/connect.py
+++ app/config/connect.py
@@ -4,7 +4,7 @@ from mongoengine import connect
 def connect_db():
     connect(
         db='fakebook',
-        # host='mongodb+srv://anhndvnist:ducanh99@cluster0.znpcr.mongodb.net',
-        host='mongodb://127.0.0.1:27017'
+        host='mongodb+srv://anhndvnist:ducanh99@cluster0.znpcr.mongodb.net',
+        # host='mongodb://127.0.0.1:27017'
 
     )
