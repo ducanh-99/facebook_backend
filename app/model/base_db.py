60d7da67464b7c474b14e7139989d21584cc8a64
ducanh <ducanhhust99@gmail.com>
2020-11-25 15:52:07 +0700

    fix

--- app/model/base_db.py
+++ app/model/base_db.py
@@ -1,15 +1,19 @@
 from mongoengine import DateField
 import mongoengine_goodjson as gj 
 import datetime
+import pytz
+d = datetime.datetime.now()
+timezone = pytz.timezone("Asia/Vientiane")
+d_aware = timezone.localize(d)
 
 class Base(gj.Document):
     meta = {'abstract': True}
     creation_date = DateField()
-    modified_date = DateField(default=datetime.datetime.now)
+    modified_date = DateField(default=d_aware)
 
     def save(self, *args, **kwargs):
         if not self.creation_date:
-            self.creation_date = datetime.datetime.now()
-        self.modified_date = datetime.datetime.now()
+            self.creation_date = d_aware
+        self.modified_date = d_aware
         return super(Base, self).save(*args, **kwargs)
     
