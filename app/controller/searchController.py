88085d66314e69ee2c9c8987fd28f3a7cdd79671
ducanh <ducanhhust99@gmail.com>
2020-12-04 12:56:16 +0700

    search

--- app/controller/searchController.py
+++ app/controller/searchController.py
@@ -6,6 +6,8 @@ import json
 
 from app.model.search import Search
 from app.model.post import Post
+from app.model.user import User
+from app.model.friends import Friend
 import app.controller.responseController as resCon
 import app.util.response as response
 
@@ -26,20 +28,40 @@ class SearchApi(Resource):
         except Exception:
             self.res = response.internal_server()
         return jsonify(result[::-1])
+
     @jwt_required
     def post(self):
         try:
             user_id = get_jwt_identity()
             body = request.get_json()
             search_all = Search.objects.get(owner=user_id)
-            Search.objects(owner=user_id).update_one(push__history_search=body["keyword"])
-            post = Post.objects(described__icontains = body["keyword"]).to_json()
-            return Response(post, mimetype="application/json", status=200)
+            Search.objects(owner=user_id).update_one(
+                push__history_search=body["keyword"])
+            users = json.loads(User.objects(
+                username__icontains=body["keyword"]).to_json())
+            post = json.loads(Post.objects(
+                described__icontains=body["keyword"]).to_json())
+            data = []
+
+            for user in users:
+                items = {}
+                items["username"] = user["username"]
+                friend = Friend.objects.get(owner=user["id"])
+                if friend.is_friend(user["id"]):
+                    items["is_friend"] = True
+                else :
+                    items["is_friend"] = False
+                data.append(items)
+
+            self.res["user"] = data
+            self.res["post"] = post
         except DoesNotExist:
             Search(owner=user_id).save()
         except Exception:
             self.res = response.internal_server()
             raise Exception
         return jsonify(self.res)
+
+
 class ListSearchApi(Resource):
     pass
